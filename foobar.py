6e990394fa27cf5692da6233fab468b1f9b202c6
Nived Sagar <nivedsagar4444@gmail.com>
2016-08-14 08:29:47 -0700

    Initial Commit

---
+++ foobar.py
@@ -0,0 +1,9 @@
+num = 3
+if(num %15 == 0):
+    print("Foobar")
+elif(num % 5 == 0):
+    print("Foo")
+elif(num % 3 == 0):
+    print("Bar")
+else:
+    print(num   )
---
+++ palindrome.py
@@ -0,0 +1,6 @@
+name = "madam"
+reverse = name[::-1]
+if (name == reverse):
+    print("It's a palindrome")
+else:
+    print("It's not")
